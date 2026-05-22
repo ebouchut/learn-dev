b24040d1132997446affb18507cbcf22b5a9a778
Eric Bouchut <ebouchut@gmail.com>
2026-05-22 11:05:11 +0200

    feat(backend): Add backend
    - Add skeleton SpringBoot App
    - Add docker compose config file that runs a container with a Postgres
    version 17 public image downloaded from Docker Hub.
    - Add an init script to create the app database and database user that
      runs on first container start.

---
+++ src/main/java/com/ericbouchut/learndev/LearnDevApplication.java
@@ -0,0 +1,13 @@
+package com.ericbouchut.learndev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnDevApplication.class, args);
+    }
+
+}
